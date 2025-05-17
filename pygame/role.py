1e9a33a66fb10461503edd4cb73fe8f5483a3aa4
MatsumotoKouta <kouta_matsumoto@demia.co.jp>
2025-05-17 15:06:27 +0900

    role.pyにボタンを追加

--- pygame/role.py
+++ pygame/role.py
@@ -3,6 +3,16 @@ import pygame
 from common import *
 
 
+BUTTON = Button(
+    font_file=JAPANESE_FONT_FILE,
+    font_size=FONT_SIZE_S,
+    x=BUTTON_X,
+    y=BUTTON_Y,
+    w=BUTTON_W,
+    h=BUTTON_H,
+)
+
+
 def draw_role_scene(player):
     pass
 
